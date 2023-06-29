60d1dc4c667b45268f75fe9cc303707872d1d7da
Mehran Basiri <Mehranbasirideveloper@gmail.com>
2023-06-29 15:19:35 +0330

    complete restaurant project

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
 
+import { SubHeading } from "../../components";
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">
+          iran krj kamalshahr bolv shahrdari golestan 1
+        </p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#DCCA87", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 06 pm</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 04 pm</p>
+      </div>
+      <button className="custom__button" style={{ marginTop: "2rem" }}>
+        Find Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find us" />
+    </div>
   </div>
 );
 
